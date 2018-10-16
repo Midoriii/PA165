aed202f49e7a596e58a015729b94a3f7252d350e
Midoriii <34740754+Midoriii@users.noreply.github.com>
2018-10-16 22:21:49 +0200

    persistence 1 completed

---
+++ eshop-persistence/src/main/java/cz/fi/muni/pa165/entity/Product.java
@@ -0,0 +1,87 @@
+package cz.fi.muni.pa165.entity;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Entity
+@Table(name = "ESHOP_PRODUCTS")
+public class Product {
+	@Id 
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private long id;
+	
+	@Column(nullable = false, unique = true)
+	private String name;
+	
+	@Column
+	private Color color;
+	
+	@Temporal(TemporalType.DATE)
+	java.util.Date addedDate;
+	
+	public Product(){}
+
+	public void setName(String given_name) {
+		this.name = given_name;	
+	}
+
+	public void setColor(Color given_color) {
+		this.color = given_color;
+	}
+
+	public void setAddedDate(Date given_date) {
+		this.addedDate = given_date;
+	}
+
+	public Object getName() {
+		return this.name;
+	}
+
+	public Object getAddedDate() {
+		return this.addedDate;
+	}
+
+	public Object getColor() {
+		return this.color;
+	}
+
+	public void setId(long given_id) {
+		this.id = given_id;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (!(obj instanceof Product))
+			return false;
+		Product other = (Product) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.getName()))
+			return false;
+		return true;
+	}
+}
