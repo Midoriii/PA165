51cc958d49d9d6376057d754b57424b2da41a71a
Midoriii <34740754+Midoriii@users.noreply.github.com>
2018-10-23 13:23:26 +0200

    persistence 2 finished

--- eshop-persistence/src/main/java/cz/fi/muni/pa165/entity/Product.java
+++ eshop-persistence/src/main/java/cz/fi/muni/pa165/entity/Product.java
@@ -1,67 +1,166 @@
 package cz.fi.muni.pa165.entity;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.Lob;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.OrderBy;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.Date;
+import cz.fi.muni.pa165.dto.Color;
+import cz.fi.muni.pa165.validation.AllOrNothing;
 
 @Entity
-@Table(name = "ESHOP_PRODUCTS")
+@AllOrNothing(members={"image", "imageMimeType"})
 public class Product {
-	@Id 
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	private long id;
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	
+
+	@Lob
+	private byte[] image;
+
+	private String imageMimeType;
+
 	@NotNull
-	@Column(nullable = false, unique = true)
+	@Column(nullable=false,unique=true)
 	private String name;
 	
-	@Column
+	/*
+	 * The day this item has been added to the eshop
+	 */
+	private LocalDate addedDate;
+
+
+	@ManyToMany
+	private Set<Category> categories = new HashSet<Category>();
+
+	@OneToOne
+	@JoinTable(name="CURRENT_PRICE")
+	private Price currentPrice;
+	
+	@OneToMany()
+	@OrderBy("priceStart DESC")
+	@JoinColumn(name="Product_FK")
+	private List<Price> priceHistory = new ArrayList<Price>();
+	
+	@Enumerated
 	private Color color;
+
 	
-	@Temporal(TemporalType.DATE)
-	Date addedDate;
+	public void setId(Long id){
+		this.id = id;
+	}
+
+
+	public void removeCategory(Category category)	{
+		this.categories.remove(category);
+	}
+
+	public void addCategory(Category c) {
+		categories.add(c);
+		c.addProduct(this);
+	}
+
+	public Set<Category> getCategories() {
+		return Collections.unmodifiableSet(categories);
+	}
+
+
+	public LocalDate getAddedDate() {
+		return addedDate;
+	}
+
+	public void setAddedDate(LocalDate addedDate) {
+		this.addedDate = addedDate;
+	}
+
+	public Product(Long productId) {
+		this.id = productId;
+	}
+	public Product() {
+	}
+	public byte[] getImage() {
+		return image;
+	}
 	
-	public Product(){}
 
-	public void setName(String given_name) {
-		this.name = given_name;	
+	public String getImageMimeType() {
+		return imageMimeType;
 	}
 
-	public void setColor(Color given_color) {
-		this.color = given_color;
+
+
+	public void setImageMimeType(String imageMimeType) {
+		this.imageMimeType = imageMimeType;
 	}
 
-	public void setAddedDate(Date Date) {
-		this.addedDate = Date;
+
+
+	public Price getCurrentPrice() {
+		return currentPrice;
 	}
 
-	public Object getName() {
-		return this.name;
-	}
 
-	public Object getAddedDate() {
-		return this.addedDate;
-	}
-
-	public Object getColor() {
-		return this.color;
-	}
-
-	public void setId(long given_id) {
-		this.id = given_id;
+	public void addHistoricalPrice(Price p){
+		priceHistory.add(p);
 	}
 	
+	public void setCurrentPrice(Price currentPrice) {
+		this.currentPrice = currentPrice;
+	}
+
+
+	public List<Price> getPriceHistory() {
+		return Collections.unmodifiableList(priceHistory);
+	}
+
+
+	public void setImage(byte[] image) {
+		this.image = image;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Color getColor() {
+		return color;
+	}
+
+	public void setColor(Color color) {
+		this.color = color;
+	}
+
+
+	public Long getId() {
+		return id;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -86,4 +185,10 @@ public class Product {
 			return false;
 		return true;
 	}
+
+
+
+	
+	
+	
 }
